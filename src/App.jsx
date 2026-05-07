89b59e8d8f3f3d3cf52a1dca845a4ad67016af80
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2026-05-07 10:26:27 +0530

    defined routes and created navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Header from './components/Header/Header';
+import { useTheme } from './utils/ContextAPI';
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Dashborad from './Screens/Dashboard/Dashborad';
 
 const App = () => {
+  const { darkMode } = useTheme();
+
   return (
-    <>
-      <h1>Todo App</h1>
-    </>
+    <div className={`${darkMode ? "dark-mode" : "light-mode"}`}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashborad />} />
+      </Routes>
+    </div>
   )
 }
 
