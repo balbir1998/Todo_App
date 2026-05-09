c8d99686c20907c5a14f37292da97ad35c735b01
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2026-05-09 11:07:19 +0530

    dark mode functionality updated

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,4 @@
-import { useTheme } from './../../utils/ContextAPI';
+import { useTheme } from './../../utils/ThemeContext';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
