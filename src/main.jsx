6ccb592f20c70f993ff607316b9f06ee092543fc
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2026-05-09 11:09:55 +0530

    Wrapped root component inside Auth Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-import ThemeProvider from './utils/ContextAPI.jsx'
+import ThemeProvider from './utils/ThemeContext'
+import AuthProvider from './utils/AuthContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
