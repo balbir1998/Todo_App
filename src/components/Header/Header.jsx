680122d36689d37ed6d2d224a749e0d9e18d706f
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2026-05-09 11:04:34 +0530

    logout functionality added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,39 @@
-import { useTheme } from './../../utils/ContextAPI';
+import { useTheme } from './../../utils/ThemeContext';
 import DescriptionIcon from '@mui/icons-material/Description';
 import SunnyIcon from '@mui/icons-material/Sunny';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../utils/AuthContext';
+import { signOut } from "firebase/auth";
+import { auth } from '../../utils/firebaseConfig';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
     const { darkMode, handleDarkMode } = useTheme();
+    const { user, setUser } = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            alert("Logout Successfully");
+
+            navigate("/");
+        } catch (err) {
+            console.error(err)
+            alert(err.message)
+        }
+    }
 
     return (
         <header className={`px-5 py-2.5 lg:px-40 lg:py-5 flex justify-between items-center
             ${darkMode ? "text-white" : "text-black"}`}
         >
-            <Link to="/" className='flex items-center gap-1.25 lg:gap-3 text-[22px] lg:text-[34px]
-            font-medium'>
+            <Link
+                to={`${user ? "/dashboard" : "/"}`}
+                className='flex items-center gap-1.25 lg:gap-3 text-[22px] lg:text-[34px] font-medium'
+            >
                 <DescriptionIcon sx={{ fontSize: 34 }} />
                 <span>TO-DO List</span>
             </Link>
@@ -27,12 +47,17 @@ const Header = () => {
                     {darkMode ? <SunnyIcon /> : <DarkModeOutlinedIcon />}
                 </div>
 
-                <div
-                    className='hover:transform hover:scale-[1.1] transition duration-300 cursor-pointer'
-                    title='Logout'
-                >
-                    <LogoutIcon className='text-[22px]! sm:text-[34px]!' />
-                </div>
+                {
+                    user && (
+                        <div
+                            className='hover:transform hover:scale-[1.1] transition duration-300 cursor-pointer'
+                            title='Logout'
+                            onClick={handleLogout}
+                        >
+                            <LogoutIcon className='text-[22px]! sm:text-[34px]!' />
+                        </div>
+                    )
+                }
             </div>
         </header >
     )
