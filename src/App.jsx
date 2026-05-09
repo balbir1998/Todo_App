c93151a5f6d9695e7af4c52d2261f4015ee7d576
balbir1998 <122143810+balbir1998@users.noreply.github.com>
2026-05-09 11:00:10 +0530

    redirecting components based on url and protected route for user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from './components/Header/Header';
-import { useTheme } from './utils/ContextAPI';
+import { useTheme } from './utils/ThemeContext';
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Dashborad from './Screens/Dashboard/Dashborad';
+import { useAuth } from './utils/AuthContext';
+import AuthHoc from './utils/AuthHoc';
 
 const App = () => {
   const { darkMode } = useTheme();
+  const { user } = useAuth();
 
   return (
     <div className={`${darkMode ? "dark-mode" : "light-mode"}`}>
       <Header />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashborad />} />
+        <Route path="/" element={user ? <Navigate to={'/dashboard'} /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to={'/dashboard'} /> : <Register />} />
+        <Route path="/dashboard" element={<AuthHoc children={<Dashborad />} />} />
       </Routes>
     </div>
   )
